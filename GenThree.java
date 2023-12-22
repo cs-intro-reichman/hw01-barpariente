a311555cf70809d4081a771205ac8e1ea2ae1c88
barpariente <154364507+barpariente@users.noreply.github.com>
2023-12-22 17:58:06 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,23 +4,23 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-	int a = Integer.parseInt(args[0]);
-	int b = Integer.parseInt(args[1]);
-   	int min, max; 
-        min = Math.min(a,b);
-        max = Math.max(a,b); 
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+   		int min, max; 
+       	 	min = Math.min(a,b);
+        	max = Math.max(a,b); 
         
 
-            int rand1 = (int)((Math.random()*(max-min+1)+min));
-            System.out.println(rand1);
-            int rand2 = (int)((Math.random()*(max-min+1)+min));
-            System.out.println(rand2);
-            int rand3 = (int)((Math.random()*(max-min+1)+min));
-            System.out.println(rand3);
+            	int rand1 = (int)((Math.random()*(max-min+1)+min));
+            	System.out.println(rand1);
+            	int rand2 = (int)((Math.random()*(max-min+1)+min));
+            	System.out.println(rand2);
+            	int rand3 = (int)((Math.random()*(max-min+1)+min));
+            	System.out.println(rand3);
 
-            int min2 = Math.min(rand1,rand2);
-            int min3 = Math.min(min2,rand3);
-            System.out.println("The minimal generated number was " + min2);
+            	int min2 = Math.min(rand1,rand2);
+           	int min3 = Math.min(min2,rand3);
+            	System.out.println("The minimal generated number was " + min2);
 
 	}
 }
