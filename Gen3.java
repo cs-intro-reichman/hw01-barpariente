e0e62193c0aa8c3bb9a12393684515b422d580c5
barpariente <154364507+barpariente@users.noreply.github.com>
2023-12-22 09:31:53 +0200

    Add files via upload

--- Gen3.java
+++ Gen3.java
@@ -8,11 +8,11 @@ public class Gen3 {
         max = Math.max(a,b); 
         
 
-            int rand1 = (int)((Math.random()*(max-min)+min));
+            int rand1 = (int)((Math.random()*(max-min+1)+min));
             System.out.println(rand1);
-            int rand2 = (int)((Math.random()*(max-min)+min));
+            int rand2 = (int)((Math.random()*(max-min+1)+min));
             System.out.println(rand2);
-            int rand3 = (int)((Math.random()*(max-min)+min));
+            int rand3 = (int)((Math.random()*(max-min+1)+min));
             System.out.println(rand3);
 
             int min2 = Math.min(rand1,rand2);
