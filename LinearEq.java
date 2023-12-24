75342eebc0deea05f8c7e3b34ddd4f9182f96866
barpariente <154364507+barpariente@users.noreply.github.com>
2023-12-24 10:23:32 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -7,7 +7,7 @@ public class LinearEq {
         
         double x = ((double)(c-b))/(a); 
 
-        System.out.println((double)(a) + " * X + " + (double)(b) + " = " + (double)(c));
-        System.out.println("X = " + x);
+        System.out.println((double)(a) + " * x + " + (double)(b) + " = " + (double)(c));
+        System.out.println("x = " + x);
     }
 }
