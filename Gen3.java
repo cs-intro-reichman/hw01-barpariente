53ebf896575db1abd7459bff619cb9f796e2bb4b
barpariente <154364507+barpariente@users.noreply.github.com>
2023-12-24 03:58:29 +0200

    Update Gen3.java

--- Gen3.java
+++ Gen3.java
@@ -2,7 +2,7 @@ public class Gen3 {
     public static void main(String[] args) {
 
         int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[1]); 
         int min, max; 
         min = Math.min(a,b);
         max = Math.max(a,b); 
